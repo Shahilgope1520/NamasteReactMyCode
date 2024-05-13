9493d55df76bb7387f1b54886333889ff43a3551
Shahilgope1520 <111412027+Shahilgope1520@users.noreply.github.com>
2024-05-13 23:15:30 +0530

    Episode 10 finished

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -44,17 +44,18 @@ const Body = () => {
     <Shimmer />
   ) : (
     <div className="body">
-      <div className="filter">
-        <div className="search">
+      <div className="filter flex">
+        <div className="search m-4 p-4">
           <input
             type="text"
-            className="search-box"
+            className="search-box border border-solid border-black rounded"
             value={searchText}
             onChange={(e) => {
               setSearchText(e.target.value);
             }}
           />
           <button
+          className="px-4 py-0.5 bg-green-100 m-4 rounded-lg"
             onClick={() => {
               // Filter the restraunt cards and update the UI
               // searchText
@@ -70,8 +71,9 @@ const Body = () => {
             Search
           </button>
         </div>
+        <div className="search m-4 p-4">
         <button
-          className="filter-btn"
+          className="px-4 py-0.5 bg-gray-100 m-4 rounded-lg"
           onClick={() => {
             const filteredList = listOfRestaurants.filter(
               (res) => res.data.avgRating > 4
@@ -81,8 +83,9 @@ const Body = () => {
         >
           Top Rated Restaurants
         </button>
+        </div>
       </div>
-      <div className="res-container">
+      <div className="flex flex-wrap">
         {filteredRestaurant.map((restaurant) => (
           <Link
             key={restaurant.info.id}
